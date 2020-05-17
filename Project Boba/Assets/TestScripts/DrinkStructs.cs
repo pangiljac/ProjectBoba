6e2641964b789eb4c4d247e2efe833b2eada6365
Jesse Angelo <pangiljac@gmail.com>
2020-05-17 01:15:11 -0700

    Basic Building Blocks of Prototype
    Starting stations for the game have been created with prototype images. Ability to switch stations while saving main player data. Basic struct is made for holding data about a boba drink.

---
+++ Project Boba/Assets/TestScripts/DrinkStructs.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Keep track of all the boba drinks data in these script
+
+public enum Flavor {
+    None = 0,
+    MilkTea = 1
+    //NumberOfTypes // Maybe use this to create barriers
+}
+
+public enum Size {
+    None = 0,
+    Wrong = 1,
+    Correct = 2
+}
+
+public enum Topping {
+    None = 0,
+    Boba = 1
+}
+
+public enum Sweetness {
+    None = 0,
+    FiftyPercent = 1,
+    HundredPercent = 2
+}
+
+public enum Ice {
+    None = 0,
+    FiftyPercent = 1,
+    HundredPercent = 2
+}
+
+public struct Drink {
+    public Size size;
+    public Flavor flavor;  
+    public Topping topping;
+    public Sweetness sweetness;
+    public Ice ice;
+}
+
+public class DrinkStructs : MonoBehaviour
+{
+    public Drink RandomizeDrink() {
+
+        Drink drink;
+
+        int enumLength = System.Enum.GetValues(typeof(Flavor)).Length;
+        drink.flavor = (Flavor)Random.Range(1, enumLength);
+        
+        enumLength = System.Enum.GetValues(typeof(Topping)).Length;
+        drink.topping = (Topping)Random.Range(0, enumLength);
+        enumLength = System.Enum.GetValues(typeof(Sweetness)).Length;
+        drink.sweetness = (Sweetness)Random.Range(0, enumLength);
+        enumLength = System.Enum.GetValues(typeof(Ice)).Length;
+        drink.ice = (Ice)Random.Range(0, enumLength);
+
+        drink.size = Size.None;
+
+        return drink;
+
+    }
+
+    public string PrintReceipt(Drink drink)
+    {
+        string receipt = "Can I get a ";
+
+        Debug.Log(drink.flavor);
+
+        switch (drink.flavor) {
+            case Flavor.MilkTea:
+                receipt = receipt + "Milk Tea, ";
+                break;
+            default:
+                receipt = "Error in Flavor";
+                return receipt;
+        }
+
+        switch (drink.sweetness) {
+            case Sweetness.None:
+                receipt = receipt + "at 0% Sweetness, ";
+                break;
+            case Sweetness.FiftyPercent:
+                receipt = receipt + "at 50% Sweetness, ";
+                break;
+            case Sweetness.HundredPercent:
+                receipt = receipt + "at 100% Sweetness, ";
+                break;
+            default:
+                receipt = "Error in Sweetness";
+                return receipt;
+
+        }
+
+        switch (drink.ice) {
+            case Ice.None:
+                receipt = receipt + "with No Ice, ";
+                break;
+            case Ice.FiftyPercent:
+                receipt = receipt + "with 50% Ice, ";
+                break;
+            case Ice.HundredPercent:
+                receipt = receipt + "with 100% Ice, ";
+                break;
+            default:
+                receipt = "Error in Ice";
+                return receipt;
+        }
+
+        switch (drink.topping) {
+            case Topping.None:
+                receipt = receipt + "and No Toppings";
+                break;
+            case Topping.Boba:
+                receipt = receipt + "and Boba";
+                break;
+            default:
+                receipt = "Error in Topping";
+                return receipt;
+        }
+
+
+        return receipt;
+
+    }
+}
---
+++ Project Boba/Assets/TestScripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public int currentStation;
+    public DrinkStructs drinkStructs;
+    public GameObject textBubble;
+
+
+    private AssetBundle myLoadedAssetBundle;
+    private string[] scenePaths;
+    private Drink receipt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("[1]") && currentStation != 1)
+        {
+            currentStation = 1;
+            SceneManager.LoadScene("Station1");
+        }
+        else if (Input.GetKeyDown("[2]") && currentStation != 2)
+        {
+            currentStation = 2;
+            SceneManager.LoadScene("Station2");
+        }
+        else if (Input.GetKeyDown("[3]") && currentStation != 3)
+        {
+            currentStation = 3;
+            SceneManager.LoadScene("Station3");
+        }
+
+        if (Input.GetKeyDown("space") && currentStation == 1) {
+            Text myText = textBubble.GetComponent<Text>();
+            receipt = drinkStructs.RandomizeDrink();
+            myText.text = drinkStructs.PrintReceipt(receipt);
+        }
+
+        if (Input.GetKey("escape")) {
+            Application.Quit();
+        }
+    }
+}
