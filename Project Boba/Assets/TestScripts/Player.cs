6c1f839d2f3294e3193fadab770259fe7c18847d
Jesse Angelo <pangiljac@gmail.com>
2020-05-23 00:35:07 -0700

    Small additions and changes
    Looks of the cup has changed and cups can be selected

--- Project Boba/Assets/TestScripts/Player.cs
+++ Project Boba/Assets/TestScripts/Player.cs
@@ -9,18 +9,42 @@ public class Player : MonoBehaviour
     public int currentStation;
     public DrinkStructs drinkStructs;
     public GameObject textBubble;
+    public int drinkHolderSize;
 
-
-    private AssetBundle myLoadedAssetBundle;
-    private string[] scenePaths;
+    private bool areDrinksSet = false;
+    private GameObject[] drinkHolder;
     private Drink receipt;
+    private string currentScene;
+
+    private Drink[] drinks;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(areDrinksSet == false) {
+            Debug.Log("Start");
+            drinks = new Drink[drinkHolderSize];
+            areDrinksSet = true;
+        }
     }
 
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+        Debug.Log("OnSceneLoaded: " + scene.name);
+        Debug.Log(mode);
+
+        currentScene = scene.name;
+
+        if (currentScene == "Station2") {
+            
+            drinkHolder = GameObject.FindGameObjectsWithTag("Cup");          
+        }
+    }
 
     // Update is called once per frame
     void Update()
@@ -50,5 +74,47 @@ public class Player : MonoBehaviour
         if (Input.GetKey("escape")) {
             Application.Quit();
         }
+
+        if (currentStation == 2) {
+            //Weird Bug that doesn't properly follow the names
+            if (Input.GetKeyDown("z"))
+            {
+                Debug.Log(drinkHolder[0].name);
+                highlightCup(0);
+            }
+            else if (Input.GetKeyDown("x"))
+            {
+                Debug.Log(drinkHolder[2].name);
+                highlightCup(2);
+            }
+            else if (Input.GetKeyDown("c"))
+            {
+                Debug.Log(drinkHolder[1].name);
+                highlightCup(1);
+            }
+        }
+        
+    }
+
+    void highlightCup(int index) {
+        GameObject highlight;
+
+        for (int x = 0; x < drinkHolder.Length; x++) {
+            
+            highlight = drinkHolder[x].transform.GetChild(4).gameObject;
+            if (x == index)
+                highlight.SetActive(true);
+            else
+                highlight.SetActive(false);
+        }
+            
+
+
+    }
+
+    void OnDisable()
+    {
+        Debug.Log("Disabled");
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
